f40ba3064450e41b014e02753351ac1df66acd56
user3 <user3@simlab27>
2019-07-15 22:00:44 +0530

    this is for coverting currency,time,distance to various units

---
+++ src/converterlibrary/distanceConverter.java
@@ -0,0 +1,42 @@
+/*To create a class distanceConverter
+ * developed by:-udaykanth67@gmail.com
+ * M.uday kanth 
+ */  
+package converterlibrary;
+import java.util.Scanner;
+public class distanceConverter
+{
+	double km,m,miles;
+    Scanner sc = new Scanner(System.in);
+    public void kmtom()
+    {
+    	System.out.print("Enter in km ");
+        km=sc.nextDouble();
+        m=(km*1000);
+        System.out.println(km+"km" +"equal to"+m+"metres");
+     }
+     public void mtokm()
+     {
+    	 System.out.print("Enter in meter ");
+         m=sc.nextDouble();
+         km=(m/1000);
+         System.out.println(m+"m" +"equal to"+km+"kilometres");
+      }
+      public void milestokm()
+      {
+    	 System.out.print("Enter in miles");
+         miles=sc.nextDouble();
+         km=(miles*1.60934);
+         System.out.println(miles+"miles" +"equal to"+km+"kilometres");
+       }
+       public void kmtomiles()
+       {
+    	   System.out.print("Enter in km");
+           km=sc.nextDouble();
+           miles=(km*0.621371);
+           System.out.println(km+"km" +"equal to"+miles+"miles");
+       }
+}
+
+
+
