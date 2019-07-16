455b2242262446e239c74c7b949b9e4f3f7d447e
user2 <user2@simlab21>
2019-07-16 21:33:34 +0530

    ConverterApp

---
+++ src/ConverterLibrary/CurrencyConverter.java
@@ -0,0 +1,50 @@
+package ConverterLibrary;
+/****
+ * Program for currency conversion
+ * @author g.vishnu
+ *  guntikavishnu@gmail.com
+ */
+public class CurrencyConverter {
+	public static double dollartoINR(double dollar)
+	{
+		double INR;
+			INR=dollar*68.56;
+		return INR;
+	}
+
+	public static double INRtodollar(double INR)
+	{
+		double dollar;
+			dollar=INR/68.56;
+		return dollar;
+	}
+	public static double EUROtoINR(double EURO)
+	{
+		double INR;
+			INR=EURO*77.39;
+		return INR;
+	}
+
+	public static double INRtoEURO(double INR)
+	{
+		double EURO;
+			EURO=INR/77.39;
+		return EURO;
+	}
+	public static double yentoINR(double yen)
+	{
+		double INR;
+			INR=yen/0.64;
+		return INR;
+	}
+
+	public static double INRtoyen(double INR)
+	{
+		double yen;
+			yen=INR*0.64;
+		return yen;
+	}
+}
+
+
+
---
+++ src/ConverterLibrary/TimeConverter.java
@@ -0,0 +1,34 @@
+package ConverterLibrary;
+/****
+ * Program for time conversion
+ * @author G.vishnu
+ *  guntikavishnu@gmail.com
+ */
+public class TimeConverter {
+	public static double hourstominutes(double hours)
+	{
+		double minutes;
+			minutes=hours*60;
+		return minutes;
+	}
+
+	public static double minutestohours(double minutes)
+	{
+		double hours;
+			hours=minutes/60;
+		return hours;
+	}
+	public static double hourstoseconds(double hours)
+	{
+		double seconds;
+			seconds=hours*3600;
+		return seconds;
+	}
+
+	public static double secondstohours(double seconds)
+	{
+		double hours;
+			hours=seconds/3600;
+		return hours;
+	}
+}
