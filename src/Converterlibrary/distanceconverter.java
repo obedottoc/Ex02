68d2733b47091a2014e1b74f95ae92fe434cf76d
user2 <user2@simlab28>
2019-07-16 21:16:29 +0530

    currencyconverter

---
+++ src/Converterlibrary/distanceconverter.java
@@ -0,0 +1,36 @@
+package Converterlibrary;
+
+public class distanceconverter {
+
+
+public static double Meter_To_Kilometer(double Meter)
+{
+double kilometer;
+kilometer=Meter*1000;
+return kilometer;
+
+}
+public static double kilometer_To_Meter(double Kilometer)
+{
+double Meter;
+Meter=Kilometer/1000;
+return Meter;
+
+}
+public static double Kilometer_To_Mile(double Kilometer)
+{
+double Miles;
+Miles=Kilometer/1.609;
+return Miles;
+
+}
+public static double Miles_To_kilometer(double Miles)
+{
+double Kilometer;
+Kilometer=Miles*1.609;
+return Kilometer;
+}
+
+
+
+}
