1e569c9e2b0f05d8bd4bfec01191d7c2fc546dfa
sumanth <sumanth@E5-571>
2019-07-25 21:18:24 +0530

    created a converter

---
+++ src/converter_library/Currencyconverter.java
@@ -0,0 +1,48 @@
+package converter_library;
+/***
+ * 
+ * @author mahesh
+ * to crate a currency converter
+ */
+public class Currencyconverter {
+	public static double Dollar_TO_Inr(double Dollar)
+	{
+	double Inr;
+	Inr=Dollar*68.56;
+	return Inr;
+
+	}
+	public static double INR_TO_Dollar(double Inr)
+	{
+	double Dollar;
+	Dollar=Inr/68.56;
+	return Dollar;
+	}
+	public static double Euro_To_Inr(double Euro)
+	{
+	double Inr;
+	Inr=Euro*77.39;
+	return Inr;
+	}
+	public static double Inr_To_Euro(double Inr)
+	{
+	double Euro;
+	Euro=Inr/77.39;
+	return Euro;
+	}
+	public static double Yen_To_Inr(double Yen)
+	{
+	double Inr;
+	Inr=Yen*0.64;
+	return Inr;
+	}
+	public static double Inr_To_Yen(double Inr)
+	{
+	double Yen;
+	Yen=Inr/0.64;
+	return Yen;
+	}
+
+	}
+
+
---
+++ src/converter_library/Distanceconverter.java
@@ -0,0 +1,41 @@
+package converter_library;
+/***
+ * 
+ * @author mahesh
+ *to create distance converter
+ */
+public class Distanceconverter {
+	public static double Meter_To_Kilometer(double Meter)
+	{
+	double kilometer;
+	kilometer=Meter*1000;
+	return kilometer;
+
+	}
+	public static double kilometer_To_Meter(double Kilometer)
+	{
+	double Meter;
+	Meter=Kilometer/1000;
+	return Meter;
+
+	}
+	public static double Kilometer_To_Mile(double Kilometer)
+	{
+	double Miles;
+	Miles=Kilometer/1.609;
+	return Miles;
+
+	}
+	public static double Miles_To_kilometer(double Miles)
+	{
+	double Kilometer;
+	Kilometer=Miles*1.609;
+	return Kilometer;
+	}
+
+
+
+	}
+
+
+
