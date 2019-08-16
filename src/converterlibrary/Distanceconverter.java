71bce74e089f7786ce7566f5f98a70cff0afa663
user3 <user3@administrator-HP-Pro-3330-MT>
2019-08-16 09:43:22 +0530

    ex-02 converterapp with pdf

---
+++ src/converterlibrary/Distanceconverter.java
@@ -0,0 +1,36 @@
+package converterlibrary;
+
+/***
+ * package for representing distance converter
+ * @author monica
+ *
+ ***/
+
+public class Distanceconverter {
+public static double metertoKM(double meter)
+{
+double KM;
+KM=meter/1000.0;
+return KM;
+}
+public static double KMtometer (double KM)
+{
+double meter;
+meter=KM*1000.0;
+return meter;
+}
+public static double milestoKM(double miles)
+{
+double KM;
+KM=miles*1.609;
+return KM;
+}
+public static double KMtomiles(double KM)
+{
+double miles;
+miles=KM/1.609;
+return miles;
+
+}
+
+}
---
+++ src/converterlibrary/TimeConverter.java
@@ -0,0 +1,35 @@
+package converterlibrary;
+/***
+ * package for representing time converter
+ * @author monica
+ *
+ ***/
+
+
+public class TimeConverter {
+public static double hourstominutes(double hours)
+{
+double minutes;
+minutes=hours*60.0;
+return minutes;
+}
+public static double minutestohours(double minutes)
+{
+double hours;
+hours=minutes/60.0;
+return hours;
+}
+public static double hourstoSeconds(double hours)
+{
+double Seconds;
+Seconds=hours*3600.0;
+return Seconds;
+}
+public static double Secondstohours(double Seconds)
+{
+double hours;
+hours=Seconds/3600.0;
+return hours;
+}
+
+}
