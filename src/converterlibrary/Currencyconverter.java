a89f35779148d29eaf213403460f8d090be32d11
user3 <user3@simlab30>
2019-08-16 20:31:03 +0530

    pdf uploaded

---
+++ src/converterlibrary/Currencyconverter.java
@@ -0,0 +1,45 @@
+package converterlibrary;
+
+public class Currencyconverter {
+
+public static double dollarToINR(double dollar)
+{
+double INR;
+INR=dollar*68.56;
+return INR;
+}
+
+public static double INRTodollar(double INR)
+{
+double dollar;
+dollar=INR/68.56;
+return dollar;
+}
+
+public static double euroToINR(double euro)
+{
+double INR;
+INR=euro*77.39;
+return INR;
+}
+
+public static double INRToeuro(double INR)
+{
+double dollar;
+dollar=INR/77.39;
+return dollar;
+}
+public static double yenToINR(double yen)
+{
+double INR;
+INR=yen*0.62;
+return INR;
+}
+
+public static double INRToyen(double INR)
+{
+double yen;
+yen=INR/68.56;
+return yen;
+}
+}
