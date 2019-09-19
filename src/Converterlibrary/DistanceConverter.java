ffc64f97fb31ba61419b52a99849f2f529880392
user2 <user2@administrator-HP-Pro-3330-MT>
2019-09-19 12:00:37 +0530

    5005

---
+++ src/Converterlibrary/DistanceConverter.java
@@ -0,0 +1,31 @@
+
+package Converterlibrary;
+
+public class DistanceConverter {
+public static double metertoKM(double meter)
+{
+double KM;
+KM=(meter/1000)+(meter%1000);
+return KM;
+}
+public static double KMtometer(double KM)
+{
+double meter;
+meter=KM*1000;
+return meter;
+}
+public static double milestoKM(double miles)
+{
+double KM;
+KM=1.609*miles;
+return KM;
+}
+public static double KMtomiles(double KM)
+{
+double miles;
+miles=(KM/1.609)+(KM%1.609);
+return miles;
+
+}
+
+}
